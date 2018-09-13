9370a83b35c0bc01d96f38e120945fcdb0136eb2
Marloes <marloes.de.krom@gmail.com>
2018-09-13 10:59:24 +0200

    eerste modules afgerond

---
+++ AcmeApp/Acme.Biz/Product.cs
@@ -0,0 +1,91 @@
+﻿using Acme.Common;
+using static Acme.Common.LoggingService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+
+    /// <summary>
+    /// Manage products carried in inventory
+    /// </summary>
+    public class Product
+    {
+        #region Constructors
+        public Product()
+        {
+            //this.ProductVendor = new Vendor();
+            Console.WriteLine("Product instance created");
+        }
+
+        public Product(int productId, string productName, string description) : this()
+        {
+            this.ProductId = productId;
+            this.ProductName = productName;
+            this.Description = description;
+
+            Console.WriteLine("Product instance has a name " + ProductName);
+        }
+        #endregion
+
+        #region Properties
+        private string productName;
+
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        private int productId;
+
+        public int ProductId
+        {
+            get { return productId; }
+            set { productId = value; }
+        }
+
+        private Vendor productVendor;
+
+        public Vendor ProductVendor
+        {
+            get {
+                if (productVendor == null)
+                {
+                    productVendor = new Vendor();
+                }
+                return productVendor;
+            }
+            set { productVendor = value; }
+        }
+
+        #endregion
+        public string SayHello()
+        {
+            //var vendor = new Vendor();
+            //vendor.SendWelcomeEmail("Message from product");
+
+            var emailService = new EmailService();
+            var confirmation = emailService.SendMessage("New Product", this.productName, "check@gmail.com" );
+
+            var result = LogAction("saying hello");
+
+            return "Hello " + ProductName +
+                " (" + ProductId + "): " +
+                Description;
+        }
+
+
+    }
+}
---
+++ AcmeApp/Tests/Acme.BizTests/ProductTests.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz.Tests
+{
+    [TestClass()]
+    public class ProductTests
+    {
+        [TestMethod()]
+        public void SayHelloTest()
+        {
+            //Arrange
+            var currentProduct = new Product();
+            currentProduct.ProductId = 1;
+            currentProduct.ProductName = "Saw";
+            currentProduct.Description = "15 - inch steel blade hand saw";
+            currentProduct.ProductVendor.CompanyName = "ABC";
+            var expected = "Hello Saw (1): 15 - inch steel blade hand saw";
+
+            //Act
+            var actual = currentProduct.SayHello();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SayHello_ParameterizedConstructor()
+        {
+            //Arrange
+            var currentProduct = new Product(1, "Saw", "15 - inch steel blade hand saw");
+            var expected = "Hello Saw (1): 15 - inch steel blade hand saw";
+
+            //Act
+            var actual = currentProduct.SayHello();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SayHello_ObjectInitializer()
+        {
+            //Arrange
+            var currentProduct = new Product
+            {
+                ProductName = "Saw",
+                ProductId = 1,
+                Description = "15 - inch steel blade hand saw"
+            };
+            var expected = "Hello Saw (1): 15 - inch steel blade hand saw";
+
+            //Act
+            var actual = currentProduct.SayHello();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Product_Null()
+        {
+            //Arrange
+
+            Product currentProduct = null;
+            //Perform null checking
+            var companyName = currentProduct?.ProductVendor?.CompanyName;
+
+            string expected = null;
+
+            //Act
+            var actual = companyName;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
